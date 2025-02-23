21b3f9e3454c51df2bbb67f737c8859ccdc9c157
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2025-02-23 19:24:29 +0100

    setup exercise, some refactor and add simple tests

--- src/test/java/model/PersonTest.java
+++ src/test/java/model/PersonTest.java
@@ -16,13 +16,31 @@ class PersonTest {
 
     @Test
     void testGetName() {
-        assertEquals("Bob", person.getName());
+        final String expectedName = "Bob";
+        assertEquals(expectedName, person.getName());
+    }
+
+    @Test
+    void testGetAge() {
+        final int expectedAge = 33;
+        assertEquals(expectedAge, person.getAge());
     }
 
     @Test
     void testUpdateAge() {
-        assertEquals(33, person.getAge());
-        person.updateAge(34);
-        assertEquals(34, person.getAge());
+        final int updatedAge = 34;
+        person.updateAge(updatedAge);
+        assertEquals(updatedAge, person.getAge());
+    }
+
+    @Test
+    void testMultipleUpdatesAge() {
+        final int numberOfUpdates = 3;
+        int updatedAge = person.getAge();
+        for (int i = 0; i < numberOfUpdates; i++) {
+            updatedAge++;
+            person.updateAge(updatedAge);
+        }
+        assertEquals(updatedAge, person.getAge());
     }
 }
